35083ef73e8e04f22a62cfe37212e39856230af7
Alex <63662620+AlexFoxD@users.noreply.github.com>
2022-08-26 11:36:31 +0300

    Initial Commit

---
+++ Task2.1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Task2.1
+//
+//  Created by neoviso on 8/26/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Task2.1/Task2_1App.swift
@@ -0,0 +1,17 @@
+//
+//  Task2_1App.swift
+//  Task2.1
+//
+//  Created by neoviso on 8/26/22.
+//
+
+import SwiftUI
+
+@main
+struct Task2_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
